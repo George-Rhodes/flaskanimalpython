43604c79283c6bbccb3f2e504c1c8d2a2ed28e11
George-Rhodes <george_rhodes@hotmail.co.uk>
2020-11-23 15:44:40 +0000

    animal task havnt tested yet

---
+++ application1/application/routes.py
@@ -0,0 +1,17 @@
+from flask import render_template, redirect, url_for
+from application import app
+import requests
+
+@app.route('/')
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('home.html', title='Home')
+
+
+@app.route('/get/animal', methods=['GET','POST'])
+def animal():
+    #response = requests.get("http://application2:5000/animal")
+    animal=requests.get('http://application2:5001/animal/name')
+    noise=requests.post('http://application2:5001/animal/noise',data=animal.text)
+    return render_template('animal.html',title='Animals', animal=animal.text, noise=noise.text)
---
+++ application2/application/routes.py
@@ -0,0 +1,34 @@
+  from flask import redirect, url_for, Response, request
+from application import app
+import requests
+import random
+
+@app.route('/')
+@app.route('/animal/name' methods=['GET','POST'])
+def animal_name():
+    animal_list=["dog","turkey","pig","cat","snake", "lion", "chicken", "robot"]
+    the_data = random.choice(animal_list)
+    return Response(the_data, mimetype='text/plain')
+
+@app.route('/animal/noise' methods=['GET','POST'])
+def animal_noise():
+    response=request.data.decode('utf-8')
+    if response == "dog":
+        noise = "bark"
+    elif response == "turkey":
+        noise = "Gobble Gobble"
+    elif response == "pig":
+        noise = "oink"
+    elif response == "cat":
+        noise = "meow"
+    elif response == "snake":
+        noise = "sssssssssssss"
+    elif response == "lion":
+        noise = "ROAR"
+    elif response == "chicken":
+        noise = "cluck cluck BWAkaaaark"
+    elif response == "robot":
+        noise = "01100001 01101100 01101100 00100000 01101000 01110101 01101101 01100001 01101110 01110011 00100000 01110111 01101001 01101100 01101100 00100000 01100100 01101001 01100101"
+    else:
+        noise = "something went wrong big man"
+    return Response(noise, mimetype='text/plain')
