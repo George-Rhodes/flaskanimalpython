f3dade9d4ac19518ba198caeab8a51da0e3c9d9d
George-Rhodes <george_rhodes@hotmail.co.uk>
2020-11-26 15:43:04 +0000

    hope

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install python3 python3-pip python3-venv
+sudo apt install python3 python3-pip python3-venv -y
 python3 -m venv venf 
 . ./venf/bin/activate
 pip3 install -r requirements.txt
